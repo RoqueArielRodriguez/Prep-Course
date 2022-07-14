3e44544ab1a4f970674c8658a346ade733f7accb
RoqueArielRodriguez <roquear10@gmail.com>
2022-07-14 15:55:06 -0300

    mi second commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -5,7 +5,7 @@ const nuevaString = "Hola";
 
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum =1;
 
 // Crea una variable booleana:
 const nuevoBool = null;
