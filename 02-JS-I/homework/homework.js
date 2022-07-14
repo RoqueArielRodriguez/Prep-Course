deb17487cded1e9afea80fa551521aca114fcb15
RoqueArielRodriguez <roquear10@gmail.com>
2022-07-14 19:20:28 -0300

    6 commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -195,8 +195,8 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
- return nombre + " " + apellido; 
-}
+  var combinado = nombre + ' ' + apellido;
+  return combinado;
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
